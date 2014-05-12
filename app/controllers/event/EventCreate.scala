0a06ecdc2cd9007052d6daf9d532edec749756dc
yugolf <yugo.maede@gmail.com>
2014-05-12 10:53:31 +0900

    イベント登録機能に必須チェックを追加（フォームの型指定によるチェック）

--- app/controllers/event/EventCreate.scala
+++ app/controllers/event/EventCreate.scala
@@ -1,42 +1,47 @@
 package controllers.event
- 
+
 import play.api._
 import play.api.mvc._
 import play.api.data.Form
 import play.api.data.Forms._
 import models.{ EventForm, Event, Events }
- 
+
 object EventCreate extends Controller {
- 
+
   /** イベントフォーム */
   val eventForm = Form(
     mapping(
-      "eventId" -> text,
-      "eventNm" -> text)(EventForm.apply)(EventForm.unapply))
- 
+      "eventId" -> nonEmptyText,
+      "eventNm" -> nonEmptyText)(EventForm.apply)(EventForm.unapply))
+
   /** 初期表示 */
   def index = Action {
     Ok(views.html.event.eventCreate(eventForm))
   }
- 
+
   /** 登録 */
   def create = Action { implicit request =>
-    val form = eventForm.bindFromRequest.get
-    val event = Event(0, form.eventId, form.eventNm)
-    Events.create(event)
-    Ok(views.html.event.eventCreate(eventForm))
+    eventForm.bindFromRequest.fold(
+      hasErrors = { form =>
+        Ok(views.html.event.eventCreate(form))
+      },
+      success = { form =>
+        val event = Event(0, form.eventId, form.eventNm)
+        Events.create(event)
+        Redirect(controllers.event.routes.EventCreate.index)
+      })
   }
- 
+
   /** テーブル作成 */
   def createTable = Action {
     Events.createTable
     Ok(views.html.event.eventCreate(eventForm))
   }
- 
+
   /** テーブル削除 */
   def dropTable = Action {
     Events.dropTable
     Ok(views.html.event.eventCreate(eventForm))
   }
- 
+
 }
