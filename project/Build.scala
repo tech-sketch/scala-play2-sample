1f7ad475aa0a6b1aeffa47424f764f4025314cfe
yugolf <yugo.maede@gmail.com>
2014-05-12 09:52:41 +0900

    データベースの接続設定（Slickライブラリの設定、H2への接続設定）を実施

---
+++ project/Build.scala
@@ -0,0 +1,16 @@
+import sbt._
+import Keys._
+import play.Project._
+
+object ApplicationBuild extends Build {
+
+  val appName = "techApp"
+  val appVersion = "1.0-SNAPSHOT"
+
+  val appDependencies = Seq(
+    "com.typesafe.slick" %% "slick" % "2.0.0"
+    )
+
+  val main = play.Project(appName, appVersion, appDependencies).settings()
+
+}
